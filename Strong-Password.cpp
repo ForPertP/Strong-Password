9b3c3787339bd9930e5ce52bbaa39ad71c59afb8
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-09-12 22:33:26 +0900

    Update Strong-Password.cpp

--- Strong-Password.cpp
+++ Strong-Password.cpp
@@ -1,1 +1,28 @@
+int minimumNumber(int n, string password)
+{
+    int count = 0;
+    int a = 0, b = 0, c = 0, d = 0;
+    
+    for (const auto & check : password )
+    {
+        if (isdigit(check))
+        {
+            a = 1;
+        }
+        else if (isupper(check))
+        {
+            b = 1;
+        }
+        else if (islower(check))
+        {
+            c = 1;
+        }
+        else
+        {
+            d = 1;
+        }
+    }
+    
+    return std::max(6 - n, 4 - a - b - c - d);
+}
 
