712250981d9db1ade310e8fa23483288927e397a
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-01-18 12:46:34 +0900

    Update Strong-Password.cpp

--- Strong-Password.cpp
+++ Strong-Password.cpp
@@ -1,3 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'minimumNumber' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. STRING password
+ */
+
 int minimumNumber(int n, string password)
 {
     int count = 0;
