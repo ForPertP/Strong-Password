f6d4b2c3c44877112118332c31fbf3c9dd3a9a5e
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-01-20 13:27:04 +0900

    Update Strong-Password.cpp

--- Strong-Password.cpp
+++ Strong-Password.cpp
@@ -97,3 +97,49 @@ int minimumNumber3(int n, string password) {
     
     return std::max(6 - n, 4 - a - b - c - d);
 }
+
+
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string password;
+    getline(cin, password);
+
+    int answer = minimumNumber(n, password);
+
+    fout << answer << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
